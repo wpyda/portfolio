265606ae6c16780d753791cce59cf0fe661dbbb3
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-03-19 00:41:41 +0100

    fa x icon styles updated

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ const contact = document.getElementById('li-contact');
 about.addEventListener('click', () => { document.querySelector('main article#about').style.display = 'block'; });
 work.addEventListener('click', () => { document.querySelector('main article#work').style.display = 'block'; });
 contact.addEventListener('click', () => { document.querySelector('main article#contact').style.display = 'block'; });
+
+// // "x" button
+// const close = document.getElementById('close');
+// const bar1 = document.createElement('div').classList.add('bar');
+// const bar2 = document.createElement('div').classList.add('bar');
+//
+// console.log('bar1', close);
+// close.appendChild(bar1);
