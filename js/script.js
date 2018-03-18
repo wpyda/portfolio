33a2d1b1372cbc1e31e94ae4ab86af624485e022
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-03-18 23:47:08 +0100

    all slides appear (functions added)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const about = document.getElementById('li-about');
-about.addEventListener('click', () => {
-    document.querySelector('main article#about').style.display = 'block';
-});
+const work = document.getElementById('li-work');
+const contact = document.getElementById('li-contact');
+
+about.addEventListener('click', () => { document.querySelector('main article#about').style.display = 'block'; });
+work.addEventListener('click', () => { document.querySelector('main article#work').style.display = 'block'; });
+contact.addEventListener('click', () => { document.querySelector('main article#contact').style.display = 'block'; });
