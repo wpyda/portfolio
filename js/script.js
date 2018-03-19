ae4ec6a145f7666d09a7305b95c4401f78fe71f0
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-03-19 23:48:41 +0100

    close slide function repaired

--- js/script.js
+++ js/script.js
@@ -11,21 +11,14 @@ about.addEventListener('click', () => openSlide('about'));
 work.addEventListener('click', () => openSlide('work'));
 contact.addEventListener('click', () => openSlide('contact'));
 
-// // "x" button
-// const close = document.querySelector('.close');
-// const bar1 = document.createElement('div').classList.add('bar');
-// const bar2 = document.createElement('div').classList.add('bar');
-//
-// console.log('bar1', close);
-// close.appendChild(bar1);
-
-// TODO check these functions, it works only with 'about'
 const closeSlide = (id) => {
     document.querySelector('main article#' + id).style.display = 'none';
     document.querySelector('header').style.display = 'block';
 };
 
-const close = document.querySelector('.close');
-close.addEventListener('click', () => closeSlide('about'));
-close.addEventListener('click', () => closeSlide('work'));
-close.addEventListener('click', () => closeSlide('contact'));
+const closeList = document.querySelectorAll('.close');
+closeList.forEach(element => {
+    element.addEventListener('click', () => closeSlide('about'));
+    element.addEventListener('click', () => closeSlide('work'));
+    element.addEventListener('click', () => closeSlide('contact'));
+});
