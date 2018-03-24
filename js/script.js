11fd266d34ac081539a2e43b5319e708c7e8bd69
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-03-24 22:46:08 +0100

    nav animation (appear + disappear) added

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ const contact = document.getElementById('li-contact');
 
 const openSlide = (id) => {
     const select = document.querySelector('main article#' + id);
+    const back = document.querySelector('header');
+
     select.classList.remove('section-disappears');
     select.classList.add('section-appears');
     select.style.display = 'block';
-    document.querySelector('header').style.display = 'none';
+
+    back.classList.remove('section-appears');
+    back.classList.add('section-disappears');
+    setTimeout(() => back.style.display = 'none', 800);
 };
 
 about.addEventListener('click', () => openSlide('about'));
@@ -16,10 +21,15 @@ contact.addEventListener('click', () => openSlide('contact'));
 
 const closeSlide = (id) => {
     const select = document.querySelector('main article#' + id);
+    const back = document.querySelector('header');
+
     select.classList.remove('section-appears');
     select.classList.add('section-disappears');
     setTimeout(() => select.style.display = 'none', 800);
-    document.querySelector('header').style.display = 'block';
+
+    back.classList.remove('section-disappears');
+    back.classList.add('section-appears');
+    back.style.display = 'block';
 };
 
 const closeList = document.querySelectorAll('.close');
